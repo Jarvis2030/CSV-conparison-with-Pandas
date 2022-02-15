6b85ce594bc876491dc727a2479c52f8ecc2ecf6
Jarvis2030 <77675271+Jarvis2030@users.noreply.github.com>
2022-02-15 22:39:46 +0800

    Update and rename Confirmation.py to Timeslot_fliter.py

--- Timeslot_fliter.py
+++ Timeslot_fliter.py
@@ -1,3 +1,9 @@
+"""
+Name: google sheet timeslot fliter
+Author: Lin Yu-Chun, June
+'Just for personal usage, still got some logical errors in this code, feel free to contact me
+for any comment or discussion'
+"""
 import pandas as pd
 Mon = pd.DataFrame()
 Tue1 = pd.DataFrame()
@@ -5,7 +11,7 @@ Tue2 = pd.DataFrame()
 Tue3 = pd.DataFrame()
 Wed1 = pd.DataFrame()
 Wed2 = pd.DataFrame()
-Thu = pd.DataFrame()
+Thu = pd.DataFrame() #create several dataframes for different timeslot
 
 
 def OutputExcel():
@@ -16,19 +22,21 @@ def OutputExcel():
 
     # SHEET1
     df_S1 = pd.DataFrame.from_dict(total)
-    df_S1.to_excel(writer, sheet_name='SHEET1', index=False)
+    df_S1.to_excel(writer, sheet_name='SHEET1', index=False) 
 
-    writer .save()
+    writer.save()
     print('成功產出'+Result_PATH)
 
 
 reg = pd.read_csv(
     r'C:\Users\user\OneDrive\桌面\hkust\UROP material\Registration form_LOOP_August 4, 2021_23.24.csv')
 
-Newreg = reg["Q5"].apply(lambda x: pd.Series(x.split(',')))
+Newreg = reg["Q5"].apply(lambda x: pd.Series(x.split(','))) 
+# google sheet collect the MCQ data by putting them in one block and split with ','
+                            
 
 for i in range(87):
-    for j in range(14):
+    for j in range(14): #Organize all the data to their timeslot dataframe
         if Newreg.iloc[i, j] == "Monday August 9":
             Mon = Mon.append(reg.iloc[i, 17:26], ignore_index=True)
 
@@ -50,7 +58,7 @@ for i in range(87):
         if Newreg.loc[i, j] == "Thursday August 12":
             Thu = Thu.append(reg.iloc[i, 17:26], ignore_index=True)
 
-Mon = Mon.reindex(Mon.Q5.str.len().sort_values().index)
+Mon = Mon.reindex(Mon.Q5.str.len().sort_values().index) #sort all the dataframe and combined them into a sheet
 Tue1 = Tue1.reindex(Tue1.Q5.str.len().sort_values().index)
 Tue2 = Tue2.reindex(Tue2.Q5.str.len().sort_values().index)
 Tue3 = Tue3.reindex(Tue3.Q5.str.len().sort_values().index)
@@ -62,6 +70,6 @@ total = pd.concat([Mon, Tue1, Tue2, Tue3, Wed1, Wed2, Thu], keys=[
                   'Monday', 'Tuesday morning', 'Tuesday noon', 'Tuesday afternoon', 'Wednesday noon', 'Wednesday afternoon', 'Thursday'],
                   names=['Session', 'Row ID'])
 
-total = total.drop(total[(total['Q4'] == "Yes") | (total['Q9'] == "No")].index)
+total = total.drop(total[(total['Q4'] == "Yes") | (total['Q9'] == "No")].index) #fliter those who disagree with the terms and conditions
 print(total)
 OutputExcel()
